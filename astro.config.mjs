a7fdee506f851bb810aa6321c11b4f8d2ce644d9
io <senpai9464@yandex.ru>
2024-10-26 13:41:16 +0000

    add

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,25 @@
 // @ts-check
-import node from '@astrojs/node';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 
+import vercel from '@astrojs/vercel/serverless';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  adapter: vercel(),
+
   server: {
     port: 3000,
     host: true,
   },
-  adapter: node({
-    mode: 'standalone',
-  }),
-  integrations: [tailwind({
-    configFile: './tailwind.config.ts',
-    nesting: true,
-  }), react()],
+  integrations: [
+    tailwind({
+      configFile: './tailwind.config.ts',
+      nesting: true,
+    }),
+    react(),
+  ],
 });
